51687fb84d66546874236971180149e31cae39df
dcsm-dsu <david.moracco@trojans.dsu.edu>
2020-10-10 08:03:06 -0500

    Initial implementation of validate

--- src/com/dmoracco/Main.java
+++ src/com/dmoracco/Main.java
@@ -3,7 +3,19 @@ package com.dmoracco;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       validate();
+    }
+
+    public static void validate(){
+        String[] algos = {"FibRecur"};
+        System.out.println("Validating Algorithms:");
+
+        for (int a = 0; a < 1; a++){
+            System.out.printf("\n\t%s\n", "Testing " + algos[a]);
+            for (int t = 0; t <= 10; t++){
+                System.out.printf("\t%d, ", FibRecur(t));
+            }
+        }
     }
 
     public static long FibRecur(long number){
