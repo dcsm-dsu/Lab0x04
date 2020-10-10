dbacdbe3e8849f351d95c7b451665638fbf02505
dcsm-dsu <david.moracco@trojans.dsu.edu>
2020-10-10 07:46:15 -0500

    Implementation of FibRecur

--- src/com/dmoracco/Main.java
+++ src/com/dmoracco/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static long FibRecur(long number){
+        if (number == 0 || number == 1) return number;
+        else return FibRecur(number-1)+FibRecur(number-2);
+    }
 }
