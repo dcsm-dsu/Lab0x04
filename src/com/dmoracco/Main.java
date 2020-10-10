87bd7ccf6ed94a5e251df322c95720feab74d001
dcsm-dsu <david.moracco@trojans.dsu.edu>
2020-10-10 10:51:29 -0500

    Implementation of FibLoop and update of validate

--- src/com/dmoracco/Main.java
+++ src/com/dmoracco/Main.java
@@ -17,11 +17,15 @@ public class Main {
         for (int s = 0; s <= 10; s++){
             System.out.printf("\t%d, ", FibCache(s));
         }
+        System.out.printf("\n\t%s\n", "Testing FibLoop");
+        for (int r = 0; r <= 10; r++){
+            System.out.printf("\t%d, ", FibLoop(r));
+        }
     }
 
 
     public static int FibRecur(int number){
-        if (number == 0 || number == 1) return number;
+        if (number < 2) return number;
         else return FibRecur(number-1)+FibRecur(number-2);
     }
 
@@ -36,11 +40,28 @@ public class Main {
     }
 
     public static int fibHelper(int number, int[] resultsTable){
-        if (number == 0 || number == 1) return number; // skip 0, 1
+        if (number < 2) return number; // skip 0, 1
         else if (resultsTable[number] > 0) return resultsTable[number]; // if result is already in table, return value
         else { // otherwise, find and fill in result, then return.
             resultsTable[number] = fibHelper(number-1, resultsTable)+fibHelper(number-2, resultsTable);
             return resultsTable[number];
         }
+    }
+
+    public static int FibLoop(int number){
+        int A = 0;
+        int B = 1;
+        int next = 0;
+
+        // skip 0, 1
+        if (number < 2) return number;
+        // Loop through storing the values for last two in sequence.
+        for (int i = 2; i <= number; i++){
+            next = A + B;
+            A = B;
+            B = next;
+        }
+
+        return next;
     }
 }
